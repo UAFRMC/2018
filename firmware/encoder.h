99669e9a52a0fe570640cb801ec23474da901ce2
Spicy Brown <achauhan@alaska.edu>
2017-10-29 16:34:31 -0800

    CHnage encoder_count_dir to signed 32 bit. Also reset drive encoders to 0 after every time we send sensors to PC

--- firmware/encoder.h
+++ firmware/encoder.h
@@ -16,7 +16,7 @@ class encoder_t{
    int value; // value from sensor
    int old_value; // Value from last read 
    uint16_t count_mono; // total number of changes seen (monotonic)
-   uint16_t count_dir;  // encoder count including up/down directions
+   int16_t count_dir;  // encoder count including up/down directions
    int16_t last_dir; // +1 for counting upward, -1 for counting downward, 0 for no direction
    
    milli_t last_change; // millis() at last changed value
@@ -53,6 +53,10 @@ class encoder_t{
         last_change=t; 
         change();
       }
+    }
+    void reset_count_dir()
+    {
+      count_dir=0;
     }
 };
 
--- firmware/robot_base.h
+++ firmware/robot_base.h
@@ -49,10 +49,11 @@ public:
 	uint32_t Mspeed:8; /// Current milliseconds per encoder tick for mining head left motor (255==stopped)
 	uint32_t Mcount:8; /// Encoder tick count for mining head left motor
 
-	uint32_t DL1count:8; /// Encoder tick count for front left drive wheel
-	uint32_t DL2count:8; /// Encoder tick count for back left drive wheel
-	uint32_t DR1count:8; /// Encoder tick count for right drive wheel
-	uint32_t DR2count:8; /// Encoder tick count for back right drive wheel
+	int32_t DL1count:9; /// Encoder tick count for front left drive wheel
+	int32_t DL2count:9; /// Encoder tick count for back left drive wheel
+	int32_t DR1count:9; /// Encoder tick count for right drive wheel
+	int32_t DR2count:9; /// Encoder tick count for back right drive wheel
+  int32_t padding:4;
 
 	int32_t Rcount:16; /// Encoder tick for bag roll motor
 
