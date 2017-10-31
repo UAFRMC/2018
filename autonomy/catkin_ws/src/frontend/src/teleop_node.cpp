8157532f10beb8dd3439a9b1a2b1ae7268f1f107
Ryker Dial <ryker.dial@gmail.com>
2017-10-31 00:31:35 -0800

    Fixed params.

--- autonomy/catkin_ws/src/frontend/src/teleop_node.cpp
+++ autonomy/catkin_ws/src/frontend/src/teleop_node.cpp
@@ -5,10 +5,11 @@
 class telop_node
 {
 public:
-	telop_node();
+	telop_node(ros::NodeHandle nh, ros::NodeHandle pr_nh);
 private:
 	void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
-	ros::NodeHandle nh;
+	ros::NodeHandle nh_;
+	ros::NodeHandle pr_nh_;
 
 	int linear_axis,angular_axis;
 	double linear_scale,angular_scale;
@@ -18,15 +19,16 @@ private:
 
 };
 
-telop_node::telop_node():linear_axis(1),angular_axis(0),linear_scale(1),angular_scale(1)
+telop_node::telop_node(ros::NodeHandle nh, ros::NodeHandle pr_nh) 
+	: nh_(nh),pr_nh_(pr_nh),linear_axis(1),angular_axis(0),linear_scale(1),angular_scale(1)
 {
-	nh.param("axis_linear",linear_axis,linear_axis);
-	nh.param("axis_angular",angular_axis,angular_axis);
-	nh.param("scale_linear",linear_scale,linear_scale);
-	nh.param("scale_linear",angular_scale,angular_scale);
+	pr_nh_.param("axis_linear",linear_axis,linear_axis);
+	pr_nh_.param("axis_angular",angular_axis,angular_axis);
+	pr_nh_.param("scale_linear",linear_scale,linear_scale);
+	pr_nh_.param("scale_linear",angular_scale,angular_scale);
 
-	velocity_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel",1);
-	joy_sub = nh.subscribe<sensor_msgs::Joy>("joy",1,&telop_node::joyCallback,this);
+	velocity_pub = nh_.advertise<geometry_msgs::Twist>("cmd_vel",1);
+	joy_sub = nh_.subscribe<sensor_msgs::Joy>("joy",1,&telop_node::joyCallback,this);
 }
 
 void telop_node::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
@@ -39,8 +41,11 @@ void telop_node::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 
 int main(int argc, char ** argv)
 {
+	// Setup the ROS node
 	ros::init(argc,argv,"telop_AuroraRobotics");
-	telop_node driver;
+	ros::NodeHandle nh;
+	ros::NodeHandle pr_nh("~"); // Private node handle to get params
+	telop_node driver(nh, pr_nh);
 
 	ros::spin();
 }
